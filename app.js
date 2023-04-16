98583fe5575c405058f902112da9ee8b89e1f1ea
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-17 06:14:02 +0900

    added logic for days

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Set express to serve static pages and images
 
-app.use("/public", express.static("public"));
+// app.use("/public", express.static("public"));
 
 // initialze body-parser
 
@@ -24,12 +24,24 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   var today = new Date();
-
-  if (today.getDay() === 6 || today.getDay() === 0)
-    res.write("<h1>Yay it's the weekend!</h1>");
-  else {
-    res.sendFile(__dirname + "/index.html");
+  var currentDay = today.getDay();
+  var day = "";
+  if (currentDay === 0) {
+    day = "Sunday";
+  } else if (currentDay === 1) {
+    day = "Monday";
+  } else if (currentDay === 2) {
+    day = "Tuesday";
+  } else if (currentDay === 3) {
+    day = "Wednesday";
+  } else if (currentDay === 4) {
+    day = "Thursday";
+  } else if (currentDay === 5) {
+    day = "Friday";
+  } else if (currentDay === 6) {
+    day = "Saturday";
   }
+  res.render("list", { kindOfDay: day });
 });
 
 // Set express to listen to port 3000 with message
