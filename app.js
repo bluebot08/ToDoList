a74a93e09b12ac3e819ec97412dcde0f7a49f0bb
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-18 06:32:37 +0900

    removed switch

--- app.js
+++ app.js
@@ -24,37 +24,17 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      // handle unexpected input
-      console.log("Error: current day is equal to: " + currentDay);
-      break;
-  }
+  var todayJapan = new Date();
 
-  res.render("list", { kindOfDay: day });
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-us", options);
+  var dayJapan = todayJapan.toLocaleDateString("ja-jp", options);
+
+  res.render("list", { kindOfDay: day, kindOfDayJapan: dayJapan });
 });
 
 // Set express to listen to port 3000 with message
